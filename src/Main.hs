ca4e429e361c3c062f49679fe3e18cfad5fe598d
James Larisch <jamesfordummies@gmail.com>
2015-08-28 16:49:53 -0400

    ants sleep now

--- src/Main.hs
+++ src/Main.hs
@@ -18,7 +18,7 @@ mapD = 700
 
 -- Data
 
-data AntState = Forage | Sleep
+data AntState = Forage | Sleep deriving Eq
 
 data Ant = Ant {
   avec :: Vector,
@@ -37,7 +37,11 @@ data World = World {
 
 -- Functions
 
+antDist :: Ant -> Ant -> Float
+antDist (Ant avec _) (Ant bvec _) = magV $ minu bvec avec
+
 moveAnt :: Food -> Ant -> Ant
+moveAnt _ a@(Ant avec Sleep) = a
 moveAnt (Food fvec) (Ant avec s) = Ant newvec s
   where newvec =  plu avec $ mulSV antV $ normalizeV $ minu fvec avec
 
@@ -48,6 +52,18 @@ spaceAnt (Ant avec _) b@(Ant bvec s)
   where diff = minu bvec avec
         modmag = (magV diff) ^ 2
         repulse = plu bvec $ mulSV 1.0 $ normalizeV diff
+
+count :: [a] -> (a -> Bool) -> Int
+count [] _ = 0
+count (a:as) p = if p a then 1 + (count as p) else count as p
+
+cyclePredAnts :: [Ant] -> [Ant] -> Float -> (Ant -> Bool) -> (Int -> Bool) -> (Ant -> Ant) -> [Ant]
+cyclePredAnts [] _ _ _ _ _ = []
+cyclePredAnts (a:as) (b:bs) fl c p t = newa:(cyclePredAnts as (bs ++ [a]) fl c p t)
+      where newa = if (p (count bs (\y -> (fl > antDist a y) && c y))) then t a else a
+
+sleepAnts :: [Ant] -> [Ant]
+sleepAnts ants = cyclePredAnts ants ants 60.0 (\x -> (state x) == Forage) (> 2) (\ a -> a { state = Sleep })
 
 cycleAnts :: [Ant] -> [Ant] -> (Ant -> Ant -> Ant) -> [Ant]
 cycleAnts [] _ _ = []
@@ -63,7 +79,8 @@ moveFood ants f@(Food fvec) newX newY
   | otherwise = f
 
 drawAnt :: Ant -> Picture
-drawAnt (Ant (x,y) s) = Translate x y (circleSolid antRadius)
+drawAnt (Ant (x,y) Sleep) = Translate x y (color red $ circleSolid antRadius)
+drawAnt (Ant (x,y) Forage) = Translate x y (color blue $ circleSolid antRadius)
 
 drawFood :: Food -> Picture
 drawFood (Food (x,y)) = Translate x y (rectangleSolid foodW foodH)
@@ -76,7 +93,7 @@ event e w = w
 
 step :: Float -> World -> World
 step f (World ants food rng) = World {
-    ants = spaceAnts $ map (moveAnt food) ants,
+    ants = sleepAnts $ spaceAnts $ map (moveAnt food) ants,
     food = moveFood ants food (clean num) (clean num'),
     numGen = rng''
   }
@@ -91,13 +108,13 @@ main =  do
           let (a, nng) = next ng
           let b = (mod a 700) - 350
           let initialWorld = World {
-            ants = [(Ant (0,0) Sleep),
-                   (Ant (40,(-30)) Sleep),
-                   (Ant ((-300),20) Sleep),
+            ants = [(Ant (0,0) Forage),
+                   (Ant (40,(-30)) Forage),
+                   (Ant ((-300),20) Forage),
                    (Ant ((-100),190) Forage),
                    (Ant ((50),220) Forage),
                    (Ant ((-300),120) Forage),
-                   (Ant ((5),320) Sleep),
+                   (Ant ((5),320) Forage),
                    (Ant ((-50),(-300)) Forage)],
             food = (Food ((realToFrac y),(realToFrac b))),
             numGen = nng
